8eb2c34b3096caa7423b51874f41a1304bc55c70
Gustavo Maldonado <gmaldo@gmail.com>
2025-07-31 20:54:01 -0300

    main tf file fix, using data correctly

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -18,7 +18,7 @@ data "azurerm_resource_group" "rg" {
 
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
   location            = var.location
   sku                 = "Basic"
   admin_enabled       = false 
@@ -27,7 +27,7 @@ resource "azurerm_container_registry" "acr" {
 resource "azurerm_log_analytics_workspace" "log" {
   name                = "${var.containerapp_name}-log"
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
@@ -35,6 +35,6 @@ resource "azurerm_log_analytics_workspace" "log" {
 resource "azurerm_container_app_environment" "env" {
   name                       = "${var.containerapp_name}-env"
   location                   = var.location
-  resource_group_name        = azurerm_resource_group.rg.name
+  resource_group_name        = data.azurerm_resource_group.rg.name
   log_analytics_workspace_id = azurerm_log_analytics_workspace.log.id
 }
