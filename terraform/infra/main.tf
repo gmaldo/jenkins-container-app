2e14cddef6904b8d34c8399ed499c13e9506e0c4
Gustavo Maldonado <gmaldo@gmail.com>
2025-07-31 21:23:33 -0300

    change admin enable

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = var.location
   sku                 = "Basic"
-  admin_enabled       = false 
+  admin_enabled       = true 
 }
 
 resource "azurerm_log_analytics_workspace" "log" {
